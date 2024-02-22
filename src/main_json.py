6999f419ccb40afd0272d8d0ba0f50b079c5f371
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-02-22 09:00:42 +0530

    Update main_json.py
    Refactored example to be in a main function()

--- src/main_json.py
+++ src/main_json.py
@@ -1,28 +1,36 @@
 import json
 from json_utils import create_corpus, create_json
 
-with open('..//problems.json','r',encoding='utf-8') as f:
-    dataset = json.load(f)
+def main():
+    with open('..//problems.json','r',encoding='utf-8') as f:
+        dataset = json.load(f)
 
-preferred_subjects = ['units-and-measurement', 'science-and-engineering-practices', 'physics']
+    preferred_subjects = ['units-and-measurement', 'science-and-engineering-practices', 'physics']
 
-dict_qa={}
-keys = list(dataset.keys())
-count_subj = 0
-count = 0
-for key in keys:
-    vals = dataset[key]
-    if vals['topic'] in preferred_subjects:
-        question = vals['hint']+vals['question'] +''.join(vals['choices'])
-        answer = vals['lecture'] + vals['solution'] if vals['solution']=="" else vals['lecture']
-        dict_qa[count_subj]={'Question':question, 'Reference':answer}
-        count_subj+=1
-    count+=1
+    dict_qa={}
+    keys = list(dataset.keys())
+    count_subj = 0
+    count = 0
+    for key in keys:
+        vals = dataset[key]
+        if vals['topic'] in preferred_subjects:
+            question = vals['hint']+vals['question'] +''.join(vals['choices'])
+            answer = vals['lecture'] + vals['solution'] if vals['solution']=="" else vals['lecture']
+            dict_qa[count_subj]={'Question':question, 'Reference':answer}
+            count_subj+=1
+        count+=1
 
-print(f'{len(list(dict_qa.keys()))} question-answer sets created')
+    print(f'{len(list(dict_qa.keys()))} question-answer sets created')
+    print('Format for a sample key value pair:')
+    for key, val in dict_qa.items():
+        print(f'Key: {key}\nValue:{val}')
+        break
+    print('\n\n\n\n',dict_qa)
+    with open('qa_json.json','w', encoding='utf-8') as fo:
+        json.dump(dict_qa,fo)
 
-with open('qa_json.json','w', encoding='utf-8') as fo:
-    json.dump(dict_qa,fo)
+    content = create_corpus(dict_qa)
+    create_json(content)
 
-content = create_corpus(dict_qa)
-create_json(content)
+if __name__ == "__main__":
+    main()
