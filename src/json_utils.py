3259e5f4901f244f7131faa6d223eae4f9b73664
Swaminathan Kannan <65940566+SwamiKannan@users.noreply.github.com>
2024-02-22 08:47:26 +0530

    Update json_utils.py
    Replaced uiud with get_id() to ensure faithfulness to generate_qa_embedding_pairs() format
    Changed index formula to reflect the expected QA dictionary input correctly
    Changed random number generator for additional indices to create corpus so that the correct list of indices is returned
    Changed final corpus formula to reflect that random.shuffle returns None and not the shuffled list.
    Created final_dict as a dict of 4 dicts to be added to output.json rather than 4 separate dicts as required by EmbeddingQAFinetuneDataset.from_json() function

--- src/json_utils.py
+++ src/json_utils.py
@@ -3,16 +3,21 @@ import json
 # from torch.utils.data import DataLoader
 # from datasets import load_dataset
 import random
-import uuid
+import pickle
 
 NUMBER_OF_CORPUS_ANSWERS = 10
+
+
+fa = open('all_used_keys.pkl','wb+')
+try:
+     all_ids = pickle.load(fa)
+except:
+    all_ids=[0]
 
 with open("D:\\Falcon_projects\\problems.json",'r') as f:
     qas = json.load(f)
 
 data = [qa[1] for qa in list(qas.items())]
-print(len(data))
-print(data[0])
 final_qas ={}
 topics = ['units-and-measurement','science-and-engineering-practices','physics']
 i=0
@@ -23,28 +28,42 @@ for d in data:
         final_qas.update({i:{'Question':question, 'Reference': reference}})
         i+=1
 
+def get_id():
+    a =[8, 4, 4, 4, 12] #Structure of ids provided in llama_index.finetuning.generate_qa_embedding_pairs()
+    x='abcdefghijklmnopqrstuvwxyz0123456789'
+    w = '-'.join([''.join(random.choices(x,weights=[1 for _ in range(len(x))],k=v)) for v in a])
+    return w
 
 def create_corpus(qa_items):
+    global all_ids
     corpus_check={}
     content = []
-    index = list(qa_items.keys())[0]
+    index = list(qa_items.keys())
     for i in index:
-        uiud_q = uuid.uuid1()
+        uiud_q, uiud_r = 0 , 0
+        while  uiud_q in all_ids:
+             uiud_q = get_id()
+        all_ids.append(uiud_q)
         question = qa_items[i]['Question']
         answer  = qa_items[i]['Reference']
-        corpora_id = random.choice(index.pop(i),NUMBER_OF_CORPUS_ANSWERS)
+        corpora_id = random.sample(index[:i]+index[i+1:],NUMBER_OF_CORPUS_ANSWERS)
         corpora_list = [qa_items[id]['Reference'] for id in corpora_id] + [answer]
-        corpus = ''.join(random.shuffle(corpora_list))
+        random.shuffle(corpora_list)
+        corpus = ''.join(corpora_list)
         if corpus in corpus_check.keys():
             corpus_check[corpus]=uiud_r
             uiud_r = corpus_check[corpus]
         else:
-            uiud_r = uuid.uuid1()
+            while uiud_r in all_ids:
+                uiud_r = get_id()
+            all_ids.append(uiud_r)
         content.append((uiud_q, question, uiud_r, corpus))
+    pickle.dump(all_ids, fa)
     del corpus_check
     return content
 
 def create_json(content_list):
+    final_dict={}
     queries ={}
     response_docs = {}
     corpus = {}
@@ -52,10 +71,13 @@ def create_json(content_list):
         queries[content[0]]=content[1]
         corpus[content[2]] = content[3]
         response_docs[content[0]]=[content[2]]
+    final_dict['queries']=queries
+    final_dict['relevant_docs']= response_docs
+    final_dict['corpus']=corpus
+    final_dict['mode']='text'
     with open('output.json','w') as f:
-        json.dump(queries)
-        json.dump(corpus)
-        json.dump(response_docs)
+        json.dump(final_dict,f)
+
 
 
 
